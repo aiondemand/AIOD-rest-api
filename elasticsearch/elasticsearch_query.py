eb73469a20a7f3f7b51574e7682cd82f95c186b2
Adrián <adrianalcoleamoreno@gmail.com>
2023-08-14 14:05:43 +0200

    elasticsearch query example

--- elasticsearch/elasticsearch_query.py
+++ elasticsearch/elasticsearch_query.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import json
 from elasticsearch import Elasticsearch
 
 ELASTIC_USER = "elastic"
-QUERY = {'match': {'title': "exotic"}}
+
+SIZE = 2
+INDEX = "publication"
+QUERY = {'match': {'platform': "example"}}
+QUERY = {'match': {'title': "in"}}
+SEARCH_AFTER = None
+SORT = {'identifier': 'asc'}
 
 def main():
+    
+    global SEARCH_AFTER
     
     # Get elasticsearch password
     with open('../.env', 'r') as f:
         for line in f:
-            if "ELASTIC_PASSWORD" in line:
+            if "ES_PASSWORD" in line:
                 ELASTIC_PASSWORD = line.split('=')[1][:-1]
                 break
     
@@ -20,8 +29,52 @@ def main():
                               basic_auth=(ELASTIC_USER, ELASTIC_PASSWORD))
     
     # Search
-    result = es_client.search(index="publication", query=QUERY)
-    print(result['hits']['hits'])
+    result = es_client.search(index=INDEX, query=QUERY, size=SIZE,
+                              search_after=SEARCH_AFTER, sort=SORT)
+    
+    # Print total number of results
+    print(f"TOTAL RESULTS: {result['hits']['total']['value']}")
+    
+    query_result = 1
+    while result['hits']['hits']:
+        
+        # Print current results
+        print(f"QUERY RESULT: {query_result}")
+        print(json.dumps(dict(result)['hits']['hits'], indent=4))
+        
+        # Actualise search_after for the next search
+        SEARCH_AFTER = result['hits']['hits'][-1]['sort']
+        query_result += 1
+        
+        # Search
+        result = es_client.search(index=INDEX, query=QUERY, size=SIZE,
+                                  search_after=SEARCH_AFTER, sort=SORT)
 
 if __name__ == "__main__":
     main()
+
+
+
+#ELASTIC_USER = "elastic"
+#QUERY = {'bool': {'must': [{'match': {'title': "Advances"}},
+#                           {'match': {'platform_identifier': "4"}}]}}
+#
+#def main():
+#
+#    # Get elasticsearch password
+#    with open('../.env', 'r') as f:
+#        for line in f:
+#            if "ES_PASSWORD" in line:
+#                ELASTIC_PASSWORD = line.split('=')[1][:-1]
+#                break
+#
+#    # Generate client
+#    es_client = Elasticsearch("http://localhost:9200",
+#                              basic_auth=(ELASTIC_USER, ELASTIC_PASSWORD))
+#
+#    # Search
+#    result = es_client.search(index="publication", query=QUERY)
+#    print(result['hits']['hits'])
+#
+#if __name__ == "__main__":
+#    main()
