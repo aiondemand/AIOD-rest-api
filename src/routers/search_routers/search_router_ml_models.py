16689bf9ab9c6f50f8d10453b918cdbf1ce753f9
taniya-das <taniyatd30@gmail.com>
2023-09-05 19:55:41 +0200

    added ml_model_router

---
+++ src/routers/search_routers/search_router_ml_models.py
@@ -0,0 +1,16 @@
+from database.model.models_and_experiments.ml_model import MLModel
+from routers.search_router import SearchRouter
+
+
+class SearchRouterMLModels(SearchRouter[MLModel]):
+    @property
+    def es_index(self) -> str:
+        return "ml_model"
+
+    @property
+    def resource_name_plural(self) -> str:
+        return "ml_models"
+
+    @property
+    def resource_class(self):
+        return MLModel
