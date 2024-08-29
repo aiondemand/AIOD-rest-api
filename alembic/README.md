7afd237e58df6af0e7a68298521ec56643e6470b
PGijsbers <p.gijsbers@tue.nl>
2024-08-29 12:19:01 +0200

    Example running migration from docker container

---
+++ alembic/README.md
@@ -0,0 +1,20 @@
+# Database Schema Migrations
+
+We use [Alembic](https://alembic.sqlalchemy.org/en/latest/tutorial.html#running-our-first-migration) to automate database schema migrations
+(e.g., adding a table, altering a column, and so on).
+Please refer to the Alembic documentation for more information.
+
+## Usage
+Commands below assume that the directory containing this readme is your current working directory.
+
+Build the image with:
+```commandline
+docker build -f Dockerfile . -t aiod-migration
+```
+
+With the sqlserver container running, you can migrate to the latest schema with:
+
+```commandline
+docker run -v $(pwd):/alembic:ro -it --network aiod_default  aiod-migration
+```
+Make sure that the specifid `--network` is the docker network that has the `sqlserver` container.
--- alembic/alembic/versions/0a23b40cc09c_extend_max_length_of_text_in_note.py
+++ alembic/alembic/versions/0a23b40cc09c_extend_max_length_of_text_in_note.py
@@ -1,8 +1,8 @@
-"""Extend char limit Note
+"""Extend max length of text in note
 
-Revision ID: 384e6782562b
+Revision ID: 0a23b40cc09c
 Revises: 
-Create Date: 2024-08-29 10:51:23.814451
+Create Date: 2024-08-29 11:37:20.827291
 
 """
 from typing import Sequence, Union
@@ -10,10 +10,9 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy import String
-from sqlalchemy.sql.type_api import TypeEngine
 
 # revision identifiers, used by Alembic.
-revision: str = "384e6782562b"
+revision: str = "0a23b40cc09c"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -28,16 +27,4 @@ def upgrade() -> None:
 
 
 def downgrade() -> None:
-    res = op.execute(
-        """
-        SELECT *
-        FROM note_organisation
-        WHERE LENGTH(value)>1800
-        """
-    )
-    breakpoint()
-    op.alter_column(
-        "note_organisation",
-        "value",
-        type_=String(1800),
-    )
+    pass
