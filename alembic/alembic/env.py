f4406de0a7e0841001417982a6c0c577167c0e23
PGijsbers <p.gijsbers@tue.nl>
2024-08-29 13:44:42 +0200

    Re-use main app database connection logic

--- alembic/alembic/env.py
+++ alembic/alembic/env.py
@@ -1,9 +1,10 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+
+# Assumes /src is in the Python path, so we can re-use logic for constructing db connections
+from database.session import db_url
+from database.session import EngineSingleton
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -38,7 +39,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = db_url()
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -57,12 +58,7 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
+    connectable = EngineSingleton().engine
     with connectable.connect() as connection:
         context.configure(connection=connection, target_metadata=target_metadata)
 
