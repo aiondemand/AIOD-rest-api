959b22dca39e92113e8579fdbbdc40e97f615ccf
Adrián <adrianalcoleamoreno@gmail.com>
2024-10-23 15:37:26 +0200

    Long strings error fixed

--- src/setup/logstash_setup/templates/sql_init.py
+++ src/setup/logstash_setup/templates/sql_init.py
@@ -7,6 +7,7 @@ TEMPLATE_SQL_INIT = """SELECT
     aiod_entry.date_modified{{extra_fields}}{{linked_fields}}
 FROM aiod.{{entity_name}}
 INNER JOIN aiod.aiod_entry ON aiod.{{entity_name}}.aiod_entry_identifier=aiod.aiod_entry.identifier
-LEFT JOIN aiod.text ON aiod.{{entity_name}}.description_identifier=aiod.text.identifier{{linked_joins}}
+LEFT JOIN aiod.text ON aiod.{{entity_name}}.description_identifier\
+=aiod.text.identifier{{linked_joins}}
 WHERE aiod.{{entity_name}}.date_deleted IS NULL{{group_by}}
 """
