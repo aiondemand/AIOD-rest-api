de2e389d53e7c9daf9b151c6003efc3b674b686d
PGijsbers <p.gijsbers@tue.nl>
2024-11-06 15:02:59 +0200

    Use dev compose to mount code directories

--- scripts/up.sh
+++ scripts/up.sh
@@ -5,4 +5,5 @@ for arg in "$@"; do
   profiles+="--profile $arg "
 done
 
-docker compose --env-file=.env --env-file=override.env ${profiles} up -d
+docker compose --env-file=.env --env-file=override.env -f docker-compose.yaml -f docker-compose.dev.yaml ${profiles} up -d
+
