dddaa7f95d9b82b53c282dfd1fec67731c027cae
PGijsbers <p.gijsbers@tue.nl>
2024-11-06 15:18:13 +0200

    Make adding the mount volumes optional through environment variable

--- scripts/up.sh
+++ scripts/up.sh
@@ -5,5 +5,14 @@ for arg in "$@"; do
   profiles+="--profile $arg "
 done
 
-docker compose --env-file=.env --env-file=override.env -f docker-compose.yaml -f docker-compose.dev.yaml ${profiles} up -d
+source .env
+source override.env
+compose_with_dev=""
+if [[ -n "${AIOD_MOUNT}" ]]; then
+  compose_with_dev="-f docker-compose.dev.yaml"
+fi
+
+command="docker compose --env-file=.env --env-file=override.env -f docker-compose.yaml ${compose_with_dev} ${profiles} up -d"
+echo "${command}"
+eval "${command}"
 
