38c817056f2189514c4c66fd8f90fad0d988546e
PGijsbers <p.gijsbers@tue.nl>
2024-11-07 10:16:32 +0200

    Add a colored message to remind the user if local changes are mounted

--- scripts/up.sh
+++ scripts/up.sh
@@ -5,11 +5,19 @@ for arg in "$@"; do
   profiles+="--profile $arg "
 done
 
+NC='\033[0m' # No Color
+CYAN='\033[1;36m'
+GREEN='\033[0;32m'
+
 source .env
 source override.env
-compose_with_dev=""
+
 if [[ -n "${AIOD_MOUNT}" ]]; then
   compose_with_dev="-f docker-compose.dev.yaml"
+  echo -e "Launching ${CYAN}with${NC} local changes."
+else
+  compose_with_dev=""
+  echo -e "Launching ${GREEN}without${NC} local changes."
 fi
 
 command="docker compose --env-file=.env --env-file=override.env -f docker-compose.yaml ${compose_with_dev} ${profiles} up -d"
