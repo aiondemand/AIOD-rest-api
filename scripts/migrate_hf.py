a9346a4d22ea725d9c4104883179de2a11f384f2
PGijsbers <p.gijsbers@tue.nl>
2024-11-18 10:48:55 +0200

    Migration script for using stable _id instead of id for HF

---
+++ scripts/migrate_hf.py
@@ -0,0 +1,59 @@
+"""
+Updates the metadata of Hugging Face entries to use `_id` instead of `id` as platform identifier.
+
+The `id` field (i.e., `username/datasetname`, e.g., `pgijsbers/titanic`) is subject to change when
+a user changes their username or the dataset name. The `_id` field is persistent across these changes,
+so can be used to avoid indexing the same dataset twice under a different platform identifier.
+"""
+import logging
+from http import HTTPStatus
+
+from sqlalchemy import select
+from database.session import DbSession, EngineSingleton
+from database.model.dataset.dataset import Dataset
+from database.model.platform.platform import Platform
+from database.model.platform.platform_names import PlatformName
+from database.model.concept.concept import AIoDConcept
+
+# Magic import which triggers ORM setup
+import database.setup
+
+import requests
+
+
+def main():
+    AIoDConcept.metadata.create_all(EngineSingleton().engine, checkfirst=True)
+    with DbSession() as session:
+        datasets_query = select(Dataset).where(Dataset.platform == PlatformName.huggingface)
+        datasets = session.scalars(datasets_query).all()
+        for dataset in datasets:
+            response = requests.get(
+                f"https://huggingface.co/api/datasets/{dataset.name}",
+                params={"full": "False"},
+                headers={},
+                timeout=10,
+            )
+            if response.status_code != HTTPStatus.OK:
+                logging.warning(f"Dataset {dataset.name} could not be retrieved.")
+                continue
+
+            dataset_json = response.json()
+            persistent_id = dataset_json["_id"]
+            if dataset.name != persistent_id:
+                logging.info(
+                    f"Dataset {dataset.name} moved to {dataset_json['id']}"
+                    "Deleting the old entry. The new entry either already exists or"
+                    "will be added on a later synchronization invocation."
+                )
+                session.delete(dataset)
+                continue
+
+            logging.info(f"Setting id of {dataset.id} to {persistent_id}")
+            dataset.platform_resource_identifier = persistent_id
+            session.add(dataset)
+            break
+        session.commit()
+
+
+if __name__ == "__main__":
+    main()
