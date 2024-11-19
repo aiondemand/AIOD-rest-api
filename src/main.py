4681714d617816bff78f79299cefbc86daae5cc7
PGijsbers <p.gijsbers@tue.nl>
2024-11-19 10:37:24 +0200

    Make sure deletion triggers also added to existing database

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ from sqlmodel import select
 
 from authentication import get_user_or_raise, User
 from config import KEYCLOAK_CONFIG
-from database.deletion.triggers import add_delete_triggers
+from database.deletion.triggers import create_delete_triggers
 from database.model.concept.concept import AIoDConcept
 from database.model.platform.platform import Platform
 from database.model.platform.platform_names import PlatformName
@@ -132,9 +132,11 @@ def create_app() -> FastAPI:
 
         drop_database = args.build_db == "drop-then-build"
         create_database(delete_first=drop_database)
-        add_delete_triggers(AIoDConcept)
         AIoDConcept.metadata.create_all(EngineSingleton().engine, checkfirst=True)
         with DbSession() as session:
+            triggers = create_delete_triggers(AIoDConcept)
+            for trigger in triggers:
+                session.execute(trigger)
             existing_platforms = session.scalars(select(Platform)).all()
             if not any(existing_platforms):
                 session.add_all([Platform(name=name) for name in PlatformName])
