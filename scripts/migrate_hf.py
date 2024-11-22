381ada3395ef3d307e3b9e870a214d0c897b9b9e
PGijsbers <p.gijsbers@tue.nl>
2024-11-22 12:58:46 +0200

    Update HF script to instead use bulk fetches and updates

--- scripts/migrate_hf.py
+++ scripts/migrate_hf.py
@@ -11,6 +11,7 @@ import logging
 import string
 from http import HTTPStatus
 import time
+from pathlib import Path
 
 from sqlalchemy import select
 from database.session import DbSession, EngineSingleton
@@ -23,11 +24,59 @@ from database.model.concept.concept import AIoDConcept
 import database.setup
 
 import requests
+import json
+
+import re
+from http import HTTPStatus
+
+
+def fetch_huggingface_metadata() -> list[dict]:
+    next_url ="https://huggingface.co/api/datasets"
+    datasets = []
+    while next_url:
+        logging.info(f"Counted {len(datasets)} so far.")
+        response = requests.get(
+            next_url,
+            params={"limit": 1000, "full": "False"},
+            headers={"Authorization":"Bearer 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"},
+        )
+        if response.status_code != HTTPStatus.OK:
+            logging.info("Stopping iteration", response.status_code, response.json())
+            break
+
+        datasets.extend(response.json())
+
+        next_info = response.headers.get('Link', '')
+        if next_url_match := re.search(r"<([^>]+)>", next_info):
+            next_url = next_url_match.group()[1:-1]
+        else:
+            next_url = None
+    return datasets
+
+
+def load_id_map():
+    HF_DATA_FILE = Path(__file__).parent / "hf_metadata.json"
+    if HF_DATA_FILE.exists():
+        logging.info(f"Loading HF data from {HF_DATA_FILE}.")
+        with open(HF_DATA_FILE, 'r') as fh:
+            hf_data = json.load(fh)
+    else:
+        logging.info("Fetching HF data from Hugging Face.")
+        hf_data = fetch_huggingface_metadata()
+        with open(HF_DATA_FILE, 'w') as fh:
+            json.dump(hf_data, fh)
+    id_map = {
+        data["id"]: data["_id"]
+        for data in hf_data
+    }
+    return id_map
 
 
 def main():
     logging.basicConfig(level=logging.INFO)
     AIoDConcept.metadata.create_all(EngineSingleton().engine, checkfirst=True)
+    id_map = load_id_map()
+
     with DbSession() as session:
         datasets_query = select(Dataset).where(Dataset.platform == PlatformName.huggingface)
         datasets = session.scalars(datasets_query).all()
@@ -40,59 +89,17 @@ def main():
     ]
     logging.info(f"Found {len(datasets)} huggingface datasets that need an update.")
 
-    for i, dataset in enumerate(datasets):
-        try:
-            response = requests.get(
-                f"https://huggingface.co/api/datasets/{dataset.name}",
-                params={"full": "False"},
-                headers={},
-                timeout=10,
-            )
-        except Exception as e:
-            logging.warning(f"Error retrieving dataset {dataset.name}: {e}")
-            continue
-
-        if response.status_code == HTTPStatus.TOO_MANY_REQUESTS:
-            logging.info(
-                f"Did not update dataset {dataset.name} as rate limit was exceeded."
-                "Rerun the script at a later time to correct this dataset's identifier."
-            )
-            # I can't find information on the dataset API fetch rate limits, this is for safety.
-            time.sleep(60)
-            continue
-
-        if response.status_code != HTTPStatus.OK:
-            logging.warning(
-                f"Dataset {dataset.name} could not be retrieved."
-                "This is almost always an indication that the dataset is removed, "
-                "so we are removing it from the index."
-            )
-            with DbSession() as session:
+    with DbSession() as session:
+        for dataset in datasets:
+            if new_id := id_map.get(dataset.platform_resource_identifier):
+                dataset.platform_resource_identifier = new_id
+                session.add(dataset)
+            else:
                 session.delete(dataset)
-            continue
+        session.commit()
+    logging.info("Done updating entries.")
 
-        dataset_json = response.json()
-        if dataset.platform_resource_identifier != dataset_json["id"]:
-            logging.info(
-                f"Dataset {dataset.platform_resource_identifier} moved to {dataset_json['id']}"
-                "Deleting the old entry. The new entry either already exists or"
-                "will be added on a later synchronization invocation."
-            )
-            with DbSession() as session:
-                session.delete(dataset)
-            continue
 
-        persistent_id = dataset_json["_id"]
-        logging.info(
-            f"Setting platform id of {dataset.platform_resource_identifier} to {persistent_id}"
-        )
-        dataset.platform_resource_identifier = persistent_id
-        with DbSession() as session:
-            session.add(dataset)
-            session.commit()
-
-        if i % 1000 == 0:
-            logging.info(f"Processed {i} of {len(datasets)} entries")
 
 
 if __name__ == "__main__":
