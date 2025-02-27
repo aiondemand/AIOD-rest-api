19fdaec21b7a7b2881308ef4b4159ba2b7696ca7
taniya-das <taniyatd30@gmail.com>
2025-02-27 14:22:37 +0100

    creating resource bundle

---
+++ src/database/model/resource_bundle/external_resource.py
@@ -0,0 +1,8 @@
+from database.model.named_relation import NamedRelation
+
+class ExternalResource(NamedRelation, table=True):
+    """
+    Stores external resources (URLs).
+    """
+    __tablename__ = "external_resource"
+
