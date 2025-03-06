5c2f49309e050a0fdf4e8b5183f3ec841ec0afbd
taniya-das <taniyatd30@gmail.com>
2025-03-06 16:08:52 +0100

    ruff formatting

--- src/database/model/computational_requirement/cpu.py
+++ src/database/model/computational_requirement/cpu.py
@@ -3,22 +3,23 @@ from sqlmodel import Field
 from database.model.concept.concept import AIoDConcept, AIoDConceptBase
 from database.model.field_length import NORMAL
 
+
 class CPUBase(AIoDConceptBase):
     """
-    The central processor of a computer system.			
+    The central processor of a computer system.
     """
-        
+
     num_cpu_cores: int | None = Field(
         description="The number of cores used by the CPU.",
         default=None,
         schema_extra={"example": 6},
-    ) 
+    )
     vendor: str | None = Field(
         description="The manufacturer of the CPU.",
         max_length=NORMAL,
         default=None,
         schema_extra={"example": "AMD"},
-    ) 
+    )
     model_name: str | None = Field(
         description="The name of the CPU model.",
         max_length=NORMAL,
@@ -34,10 +35,9 @@ class CPUBase(AIoDConceptBase):
     clock_speed: int | None = Field(
         description="Base clock speed of the CPU in MHz.",
         default=None,
-        schema_extra={"example": 3600}
+        schema_extra={"example": 3600},
     )
-    
-    
+
+
 class CPU(CPUBase, AIoDConcept, table=True):  # type: ignore [call-arg]
     __tablename__ = "cpu"
-    
