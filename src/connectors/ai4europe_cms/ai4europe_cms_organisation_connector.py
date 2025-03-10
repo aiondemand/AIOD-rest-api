5bb9e4f1bf7dd2f5bde710810d8a8a73dda7ac09
aganios <ant.ganios@gmail.com>
2025-03-10 12:52:29 +0200

    update ai4europe_cms organisation connector

--- src/connectors/ai4europe_cms/ai4europe_cms_organisation_connector.py
+++ src/connectors/ai4europe_cms/ai4europe_cms_organisation_connector.py
@@ -1,27 +1,21 @@
-import dateutil.parser
 import logging
 import requests
-from datetime import datetime, timedelta, timezone
 
 from requests.exceptions import HTTPError
-from sqlmodel import SQLModel
-from typing import Iterator, Tuple
-from config import REQUEST_TIMEOUT
+from typing import Iterator
 
 from connectors.abstract.resource_connector import ResourceConnector
 from connectors.record_error import RecordError
-from database.model.concept.aiod_entry import AIoDEntryCreate,AIoDEntryORM
+from database.model.concept.aiod_entry import AIoDEntryCreate
 from database.model.platform.platform_names import PlatformName
 from database.model.resource_read_and_create import resource_create
 from database.model.agent.organisation import Organisation
 from database.model.agent.contact import Contact
-from database.model.agent.email import Email
 from database.model.agent.location import LocationORM, AddressORM, GeoORM
 from connectors.resource_with_relations import ResourceWithRelations
 
+
 class AI4EuropeCmsOrganisationConnector(ResourceConnector[Organisation]):
-
-
     @property
     def resource_class(self) -> type[Organisation]:
         return Organisation
@@ -31,19 +25,15 @@ class AI4EuropeCmsOrganisationConnector(ResourceConnector[Organisation]):
         return PlatformName.ai4europe_cms
 
     def run(
-            self, state: dict, **kwargs
-    ) -> Iterator[Tuple[ Organisation | ResourceWithRelations[Organisation] | RecordError]]:
+        self, state: dict, **kwargs
+    ) -> Iterator[ResourceWithRelations[Organisation] | RecordError]:
         """Fetch resources and update the state"""
 
-        url_data = (
-            "https://community-dev-api.aiod.eu/api/organisations/"
-        )
+        url_data = "https://community-dev-api.aiod.eu/api/organisations/"
+
         headers = {"AuthorizationToken": "1234567890"}
 
-        response = requests.get(
-            url_data, 
-            headers=headers,
-        )
+        response = requests.get(url_data, headers=headers, timeout=600)
 
         if not response.ok:
             status_code = response.status_code
@@ -53,160 +43,157 @@ class AI4EuropeCmsOrganisationConnector(ResourceConnector[Organisation]):
             err = HTTPError(err_msg)
             yield RecordError(identifier=None, error=err)
             return
-        
+
         try:
             organisations = response.json()
         except Exception as e:
             yield RecordError(identifier=None, error=e)
             return
 
-
         for organisation in organisations:
-      
+
+            pydantic_class_contact = resource_create(Contact)
             pydantic_class = resource_create(Organisation)
-            pydantic_class.__fields__["contact_details"].type_ = Contact
-
-
             contact_data = organisation["contact_details"]
 
-            contact = Contact(
-                name=contact_data["name"] if contact_data.get("name") is not None else None,
-                platform=contact_data["platform"] if contact_data.get("platform") is not None else None,
-                platform_resource_identifier=(
-                    contact_data["platform_resource_identifier"]
-                    if contact_data.get("platform_resource_identifier") is not None else None
-                ),
-                email=(
-                    [Email(name=e) for e in contact_data["email"] if e is not None]
-                    if contact_data.get("email") is not None else []
-                ),
-                aiod_entry=(
-                    AIoDEntryORM(
-                        status=organisation["contact_details"]["aiod_entry"]["status"]
+            yield ResourceWithRelations[Organisation](
+                resource=pydantic_class(
+                    aiod_entry=(
+                        AIoDEntryCreate()
                         if (
-                            organisation.get("contact_details") is not None and
-                            organisation["contact_details"].get("aiod_entry") is not None and
-                            organisation["contact_details"]["aiod_entry"].get("status") is not None
-                        ) else None
-                    )
-                    if organisation.get("contact_details") is not None and
-                    organisation["contact_details"].get("aiod_entry") is not None else None
-                ),
-                location=(
-                    [
-                        LocationORM(
-                            geo=GeoORM(
-                                latitude=(
-                                    l["geo"]["latitude"]
-                                    if l.get("geo") is not None and l["geo"].get("latitude") is not None
-                                    else None
-                                ),
-                                longitude=(
-                                    l["geo"]["longitude"]
-                                    if l.get("geo") is not None and l["geo"].get("longitude") is not None
-                                    else None
-                                ),
-                                elevation_millimeters=(
-                                    l["geo"]["elevation_millimeters"]
-                                    if l.get("geo") is not None and l["geo"].get("elevation_millimeters") is not None
-                                    else None
-                                ),
-                            ),
-                            address=AddressORM(
-                                region=(
-                                    l["address"]["region"]
-                                    if l.get("address") is not None and l["address"].get("region") is not None
-                                    else None
-                                ),
-                                locality=(
-                                    l["address"]["locality"]
-                                    if l.get("address") is not None and l["address"].get("locality") is not None
-                                    else None
-                                ),
-                                street=(
-                                    l["address"]["street"]
-                                    if l.get("address") is not None and l["address"].get("street") is not None
-                                    else None
-                                ),
-                                postal_code=(
-                                    l["address"]["postal_code"]
-                                    if l.get("address") is not None and l["address"].get("postal_code") is not None
-                                    else None
-                                ),
-                                address=(
-                                    l["address"]["address"]
-                                    if l.get("address") is not None and l["address"].get("address") is not None
-                                    else None
-                                ),
-                                country=(
-                                    l["address"]["country"]
-                                    if l.get("address") is not None and l["address"].get("country") is not None
-                                    else None
-                                ),
-                            )
+                            organisation.get("contact_details") is not None
+                            and organisation["contact_details"].get("aiod_entry") is not None
                         )
-                        for l in contact_data["location"] if l is not None
+                        else None
+                    ),
+                    platform_resource_identifier=(
+                        organisation["platform_resource_identifier"]
+                        if organisation.get("platform_resource_identifier") is not None
+                        else None
+                    ),
+                    platform=organisation["platform"]
+                    if organisation.get("platform") is not None
+                    else None,
+                    name=organisation["name"] if organisation.get("name") is not None else None,
+                    date_published=organisation["date_published"]
+                    if organisation.get("date_published") is not None
+                    else None,
+                    scientific_domain=[sd for sd in organisation.get("scientific_domain")]
+                    if organisation.get("scientific_domain") is not None
+                    else [],
+                    industrial_sector=[ins for ins in organisation.get("industrial_sector")]
+                    if organisation.get("industrial_sector") is not None
+                    else [],
+                    relevant_link=[rl for rl in organisation.get("relevant_link")]
+                    if organisation.get("relevant_link") is not None
+                    else [],
+                    alternate_name=[an for an in organisation.get("alternate_name")]
+                    if organisation.get("alternate_name") is not None
+                    else [],
+                    application_area=[ar for ar in organisation.get("application_area")]
+                    if organisation.get("application_area") is not None
+                    else [],
+                    keyword=[k for k in organisation.get("keyword")]
+                    if organisation.get("keyword") is not None
+                    else [],
+                    same_as=organisation["same_as"]
+                    if organisation.get("same_as") is not None
+                    else None,
+                    legal_name=organisation["legal_name"]
+                    if organisation.get("legal_name") is not None
+                    else None,
+                    ai_relevance=organisation["ai_relevance"]
+                    if organisation.get("ai_relevance") is not None
+                    else None,
+                    type=organisation["type"] if organisation.get("type") is not None else None,
+                ),
+                resource_ORM_class=Organisation,
+                related_resources={
+                    "contact": [
+                        pydantic_class_contact(
+                            name=contact_data["name"]
+                            if contact_data.get("name") is not None
+                            else None,
+                            platform=contact_data["platform"]
+                            if contact_data.get("platform") is not None
+                            else None,
+                            platform_resource_identifier=(
+                                contact_data["platform_resource_identifier"]
+                                if contact_data.get("platform_resource_identifier") is not None
+                                else None
+                            ),
+                            email=(
+                                [e for e in contact_data["email"] if e is not None]
+                                if contact_data.get("email") is not None
+                                else []
+                            ),
+                            location=[
+                                LocationORM(
+                                    geo=GeoORM(
+                                        latitude=(
+                                            loc["geo"]["latitude"]
+                                            if loc.get("geo") is not None
+                                            and loc["geo"].get("latitude") is not None
+                                            else None
+                                        ),
+                                        longitude=(
+                                            loc["geo"]["longitude"]
+                                            if loc.get("geo") is not None
+                                            and loc["geo"].get("longitude") is not None
+                                            else None
+                                        ),
+                                        elevation_millimeters=(
+                                            loc["geo"]["elevation_millimeters"]
+                                            if loc.get("geo") is not None
+                                            and loc["geo"].get("elevation_millimeters") is not None
+                                            else None
+                                        ),
+                                    ),
+                                    address=AddressORM(
+                                        region=(
+                                            loc["address"]["region"]
+                                            if loc.get("address") is not None
+                                            and loc["address"].get("region") is not None
+                                            else None
+                                        ),
+                                        locality=(
+                                            loc["address"]["locality"]
+                                            if loc.get("address") is not None
+                                            and loc["address"].get("locality") is not None
+                                            else None
+                                        ),
+                                        street=(
+                                            loc["address"]["street"]
+                                            if loc.get("address") is not None
+                                            and loc["address"].get("street") is not None
+                                            else None
+                                        ),
+                                        postal_code=(
+                                            loc["address"]["postal_code"]
+                                            if loc.get("address") is not None
+                                            and loc["address"].get("postal_code") is not None
+                                            else None
+                                        ),
+                                        address=(
+                                            loc["address"]["address"]
+                                            if loc.get("address") is not None
+                                            and loc["address"].get("address") is not None
+                                            else None
+                                        ),
+                                        country=(
+                                            loc["address"]["country"]
+                                            if loc.get("address") is not None
+                                            and loc["address"].get("country") is not None
+                                            else None
+                                        ),
+                                    ),
+                                )
+                                for loc in contact_data["location"]
+                                if loc is not None
+                            ]
+                            if contact_data.get("location") is not None
+                            else [],
+                        )
                     ]
-                    if contact_data.get("location") is not None else []
-                )
-            )
-
-            org = pydantic_class(
-                aiod_entry=(
-                    AIoDEntryCreate()
-                    if (
-                        organisation.get("contact_details") is not None and
-                        organisation["contact_details"].get("aiod_entry") is not None
-                    ) else None
-                ),
-                platform_resource_identifier=(
-                    organisation["platform_resource_identifier"]
-                    if organisation.get("platform_resource_identifier") is not None else None
-                ),
-                platform=organisation["platform"] if organisation.get("platform") is not None else None,
-                name=organisation["name"] if organisation.get("name") is not None else None,
-                date_published=organisation["date_published"]
-                    if organisation.get("date_published") is not None else None,\
-
-                scientific_domain=[
-                   sd for sd in organisation.get("scientific_domain") 
-                ] if organisation.get("scientific_domain") is not None else [],
-
-                 industrial_sector=[
-                   ins for ins in organisation.get("industrial_sector") 
-                ] if organisation.get("industrial_sector") is not None else [],
-
-                 relevant_link=[
-                   rl for rl in organisation.get("relevant_link") 
-                ] if organisation.get("relevant_link") is not None else [],
-
-                 alternate_name=[
-                   an for an in organisation.get("alternate_name") 
-                ] if organisation.get("alternate_name") is not None else [],
-
-
-                 application_area=[
-                   ar for ar in organisation.get("application_area") 
-                ] if organisation.get("application_area") is not None else [],
-
-    
-                 keyword=[
-                   k for k in organisation.get("keyword") 
-                ] if organisation.get("keyword") is not None else [],
-
-
-                same_as=organisation["same_as"]
-                    if organisation.get("same_as") is not None else None,
-                legal_name=organisation["legal_name"]
-                    if organisation.get("legal_name") is not None else None,
-                ai_relevance=organisation["ai_relevance"]
-                    if organisation.get("ai_relevance") is not None else None,
-                type=organisation["type"]
-                    if organisation.get("type") is not None else None,
-                contact_details=contact if contact is not None else None
-            )
-            # yield org
-            yield ResourceWithRelations[pydantic_class](
-                resource=org, 
-                resource_ORM_class=Organisation, 
+                },
             )
--- src/tests/connectors/ai4europe_cms/test_ai4europe_cms_organisation_connector.py
+++ src/tests/connectors/ai4europe_cms/test_ai4europe_cms_organisation_connector.py
@@ -3,15 +3,14 @@ import json
 import responses
 
 from datetime import datetime
-from requests.exceptions import HTTPError
 
-from connectors.ai4europe_cms.ai4europe_cms_organisation_connector import AI4EuropeCmsOrganisationConnector #.aibuilder_mlmodel_connector import AIBuilderMLModelConnector
+from connectors.ai4europe_cms.ai4europe_cms_organisation_connector import (
+    AI4EuropeCmsOrganisationConnector,
+)
 from connectors.resource_with_relations import ResourceWithRelations
-from connectors.record_error import RecordError
 from database.model.agent.organisation import Organisation
 from database.model.platform.platform_names import PlatformName
 from tests.testutils.paths import path_test_resources
-from database.model.ai_resource.text import Text
 
 
 TOKEN = "TEST_AIBUILDER_API_TOKEN"
@@ -20,21 +19,20 @@ connector = AI4EuropeCmsOrganisationConnector()
 test_resources_path = os.path.join(path_test_resources(), "connectors", "ai4europe_cms")
 
 
-
 def test_run_happy_path():
     catalog_list_path = os.path.join(test_resources_path, "organisations_list.json")
-    
+
     with responses.RequestsMock() as mocked_requests:
         with open(catalog_list_path, "r") as f:
             expected_response = json.load(f)
-        
+
         with responses.RequestsMock() as mocked_requests:
             mocked_requests.add(
-                responses.GET, 
+                responses.GET,
                 API_URL,
-                json=expected_response, 
+                json=expected_response,
                 headers={"AuthorizationToken": "1234567890"},
-                status=200
+                status=200,
             )
             fetched_resources = list(connector.run(state={}))
 
@@ -48,4 +46,3 @@ def test_run_happy_path():
             assert resource.resource.name == f"mock-organisation-{i + 1}"
             assert resource.resource.date_published == mocked_datetime
             assert resource.resource.type == f"mock-type-{i + 1}"
-        
