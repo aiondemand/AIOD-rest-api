f012558c1f121872be8f65d6c8bd63087a4a1fb6
taniya-das <taniyatd30@gmail.com>
2025-06-13 13:47:58 +0200

    revert concept.py to original form

--- src/database/model/concept/concept.py
+++ src/database/model/concept/concept.py
@@ -80,26 +80,6 @@ class AIoDConcept(AIoDConceptBase):
         unique=True,
     )
 
-    platform: str | None = Field(
-        max_length=SHORT,
-        default=None,
-        description="The external platform from which this resource originates. Leave empty if "
-        "this item originates from AIoD. If platform is not None, the "
-        "platform_resource_identifier should be set as well.",
-        schema_extra={"example": PlatformName.example},
-        foreign_key="platform.name",
-    )
-
-    platform_resource_identifier: str | None = Field(
-        max_length=NORMAL,
-        description="A unique identifier issued by the external platform that's specified in "
-        "'platform'. Leave empty if this item is not part of an external platform. For example, "
-        "for HuggingFace, this should be the <namespace>/<dataset_name>, and for Openml, the "
-        "OpenML identifier.",
-        default=None,
-        schema_extra={"example": "1"},
-    )
-
     aiod_entry: AIoDEntryORM = Relationship()
 
     _id_generator: Callable[[], str] | None = None
