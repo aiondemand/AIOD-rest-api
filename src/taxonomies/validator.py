3e57fb39de02dbbb455a4d5c9807ce136709accc
PGijsbers <p.gijsbers@tue.nl>
2025-06-15 13:02:43 +0200

    Clearer error message when taxonomy is not defined

--- src/taxonomies/validator.py
+++ src/taxonomies/validator.py
@@ -1,10 +1,14 @@
 from pydantic import validator
 from .loader import get_taxonomy_map
 
+
 def taxonomy_field_validator(field_name: str):
     def _validator(cls, v):
-        allowed = get_taxonomy_map().get(field_name, [])
+        allowed = get_taxonomy_map().get(field_name)
+        if allowed is None:
+            raise KeyError(f"No taxonomy defined for {field_name!r}.")
         if v not in allowed:
             raise ValueError(f"'{v}' is not a valid {field_name!r}.")
         return v
+
     return validator(field_name, allow_reuse=True)(_validator)
