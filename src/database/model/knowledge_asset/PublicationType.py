eb7c99697695ab614d1d1fba7df43bbc1ca07979
PGijsbers <p.gijsbers@tue.nl>
2025-06-25 14:36:07 +0200

    Create hierarchical structure for publication type

--- src/database/model/knowledge_asset/PublicationType.py
+++ src/database/model/knowledge_asset/PublicationType.py
@@ -1,5 +1,22 @@
+from typing import Self, Optional
+
+from sqlalchemy.orm import backref
+from sqlmodel import Field, Relationship
+
 from database.model.named_relation import Taxonomy
 
 
 class PublicationType(Taxonomy, table=True):  # type: ignore [call-arg]
     __tablename__ = "publication_type"
+
+    # Taxonomies are hierarchical, e.g., a Cow is also a Mammal.
+    parent_id: int | None = Field(
+        foreign_key="publication_type.identifier", default=None, nullable=True
+    )
+    children: list["PublicationType"] = Relationship(
+        sa_relationship_kwargs=dict(
+            cascade="all",
+            backref=backref("parent", remote_side="PublicationType.identifier"),
+        )
+    )
+    # parent: Optional['PublicationType'] = Relationship()
