7a441b840a61ec7d56862ce9b39fbc448bbf5db4
PGijsbers <p.gijsbers@tue.nl>
2025-07-29 17:17:41 +0200

    Add a create endpoint

--- src/routers/new_resource_router.py
+++ src/routers/new_resource_router.py
@@ -1,15 +1,17 @@
 from http import HTTPStatus
 from http.client import HTTPException
+from typing import Callable
 
 from fastapi import APIRouter, Depends
 from sqlmodel import SQLModel, Session
 
-from authentication import KeycloakUser, get_user_or_none
+from authentication import KeycloakUser, get_user_or_none, get_user_or_raise
 from database.authorization import user_can_read
 from error_handling.error_handling import as_http_exception
 from database.model.concept.aiod_entry import EntryStatus
 from database.model.resource_read_and_create import resource_read, resource_create
 from database.session import DbSession, get_session
+from database.model.serializers import deserialize_resource_relationships
 
 
 # Version?
@@ -20,11 +22,17 @@ def create_router(
     database_model: type[SQLModel],
     resource_name: str,
     resource_name_plural: str,
+    orm_to_read_schema: Callable[[type[SQLModel]], type[SQLModel]] = resource_read,
+    orm_to_create_schema: Callable[[type[SQLModel]], type[SQLModel]] = resource_create,
+    orm_to_read_object: Callable[[SQLModel], SQLModel] | None = None,
+    create_object_to_orm: Callable[[SQLModel], SQLModel] | None = None,
 ) -> APIRouter:
     app = APIRouter()
 
-    view_model = _from_database_to_read_schema(database_model)
-    # create_model = _database_model_to_create_view(database_model)
+    view_model = orm_to_read_schema(database_model)
+    create_model = orm_to_create_schema(database_model)
+    orm_to_read_object = orm_to_read_object or view_model.model_validate
+    create_object_to_orm = create_object_to_orm or database_model.model_validate
 
     @app.get(
         f"/{resource_name_plural}/{{identifier}}",
@@ -40,11 +48,29 @@ def create_router(
         resource = session.get(database_model, identifier)
         # todo: convert schema
         result = view_model.from_orm(resource)
+        return result
         if user is None and resource.aiod_entry.status != EntryStatus.PUBLISHED:
             raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED)
         if user_can_read(user, resource.aiod_entry):
             return result
         raise HTTPException(status_code=HTTPStatus.FORBIDDEN)
+
+    @app.post(
+        f"/{resource_name_plural}",
+        name=resource_name,
+        description=f"Register a {resource_name}.",
+    )
+    def create_resource(
+        asset: create_model,
+        user: KeycloakUser | None = Depends(get_user_or_raise),
+        session: Session = Depends(get_session),
+    ) -> view_model:
+        resource = create_object_to_orm(asset)
+        deserialize_resource_relationships(session, database_model, resource, asset, user)
+        session.add(resource)
+        # add logic for ensuring platform resource identifiers and the likes
+        session.commit()
+        return orm_to_read_object(resource)
 
     ...  # other methods
 
@@ -68,3 +94,8 @@ def _from_database_to_read_schema(database_schema: type[SQLModel]) -> type[SQLMo
 
 
 def _from_create_to_database_object(resource: SQLModel) -> SQLModel: ...
+
+
+# AddedFieldRouter = create_router(...)
+# RemovedFieldRouter = create_router(...)
+# ChangedFieldRouter = create_router(...)
