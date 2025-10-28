4323ecb1d83c857a863f4b0b2791cd58d84d2d3e
PGijsbers <p.gijsbers@tue.nl>
2025-10-28 15:00:05 +0200

    Add test for adding user by nickname

--- src/tests/testutils/mock_keycloak.py
+++ src/tests/testutils/mock_keycloak.py
@@ -38,17 +38,3 @@ def _mocked_user(path_user_json: Path) -> responses.RequestsMock:
             json=response,
         )
         yield request_mock
-
-
-def _mocked_admin() -> responses.RequestsMock:
-    cached_response = path_test_resources() / "authentication" / "admin_connect.json"
-    with cached_response.open("r") as f:
-        response = json.load(f)
-
-    with responses.RequestsMock() as request_mock:
-        request_mock.add(
-            responses.POST,
-            "http://keycloak:8080/aiod-auth/realms/master/protocol/openid-connect/token",
-            json=response,
-        )
-        yield request_mock
