b59e97200a55dc4c2ce8169dc8d71a67cd6a626c
Shreyansh Singh <shreyanshch098@gmail.com>
2026-02-21 19:56:23 +0530

    fix-deletition problem

--- src/database/deletion/triggers.py
+++ src/database/deletion/triggers.py
@@ -191,7 +191,7 @@ def create_deletion_trigger_many_to_many(
         f"""
         NOT EXISTS (
                 SELECT 1 FROM {link_name}
-                WHERE {link_name}.{link_to_identifier} = {delete_name}.{to_delete_identifier}
+                WHERE {link_name}.{link_to_identifier} = OLD.{link_to_identifier}
         )
         """  # noqa: S608  # never user input
         for link_name in link_names
