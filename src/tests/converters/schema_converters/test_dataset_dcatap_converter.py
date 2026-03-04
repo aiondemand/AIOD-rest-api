2abc93f0cc40be9d91d99cbc11b910743ad2bd79
Satvik Mishra <112589278+satvshr@users.noreply.github.com>
2026-03-04 13:16:02 +0530

    fixes

--- src/tests/converters/schema_converters/test_dataset_dcatap_converter.py
+++ src/tests/converters/schema_converters/test_dataset_dcatap_converter.py
@@ -31,7 +31,9 @@ def test_aiod_to_dcatap_happy_path(dataset: Dataset):
 
     with open(path_test_resources() / "schemes" / "dcatap" / "dataset.json", "r") as f:
         expected = f.read()
-    for i, (row_actual, row_expected) in enumerate(zip(actual.split("\n"), expected.split("\n"))):
-        assert row_actual == row_expected, f"Line {i}: {row_actual} != {row_expected}"
+    for i, (row_actual, row_expected) in enumerate(
+        zip(actual.split("\n"), expected.split("\n"), strict=False)
+        ):
+        assert row_actual == row_expected, f"Line {i}: {row_actual} != {row_expected}"  # noqa: S101
     # Checked-in files have a newline at end of file, which we do not care for here:
-    assert actual == expected[:-1]
+    assert actual == expected[:-1]  # noqa: S101
